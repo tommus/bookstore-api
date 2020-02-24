9d0675a5a5bc3a8cb37513de1cc2431a96f7873b
Tomasz Dzieniak <tomasz.dzieniak@windly.co>
2020-02-24 11:23:37 +0100

    Revert dev configuration

--- bookstore/settings/dev.py
+++ bookstore/settings/dev.py
@@ -12,21 +12,14 @@ INSTALLED_APPS += [
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(ROOT_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bookstore",
+        "USER": "tommus",
+        "PASSWORD": "b0248d59e50ae772a0fd4cc16591fb24",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "bookstore",
-#         "USER": "tommus",
-#         "PASSWORD": "b0248d59e50ae772a0fd4cc16591fb24",
-#         "HOST": "localhost",
-#         "PORT": "5432",
-#     }
-# }
 
 # endregion
 
