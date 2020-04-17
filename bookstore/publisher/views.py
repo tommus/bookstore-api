bd34b98eab5b7b3c16e95d73b7f757bae83850b9
Tomasz Dzieniak <tomasz.dzieniak@windly.co>
2020-04-17 17:34:28 +0200

    Correct publishers API docs

--- bookstore/publisher/views.py
+++ bookstore/publisher/views.py
@@ -1,9 +1,14 @@
 from django.utils.decorators import method_decorator
+from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
+from rest_framework.status import HTTP_200_OK, HTTP_404_NOT_FOUND
 
 from bookstore.common.pagination import CursorHashPagination
+from bookstore.common.schemas import schema_error
 from bookstore.publisher.models import Publisher
+from bookstore.publisher.schemas import schema_publisher_list_query, schema_publisher_list_response, \
+    schema_publisher_details_path, schema_publisher_details_response
 from bookstore.publisher.serializers import PublisherSerializer
 
 
@@ -11,6 +16,7 @@ class PublisherPagination(CursorHashPagination):
     page_size = 25
 
 
+# noinspection PyTypeChecker
 @method_decorator(
     name="create",
     decorator=swagger_auto_schema(auto_schema=None)
@@ -22,9 +28,17 @@ class PublisherPagination(CursorHashPagination):
 @method_decorator(
     name="list",
     decorator=swagger_auto_schema(
-        operation_id="List publishers",
+        manual_parameters=schema_publisher_list_query,
+        operation_id="publisher:list",
         operation_summary="List publishers (pageable)",
         operation_description="Allows to retrieve a list of publishers.",
+        responses={
+            HTTP_200_OK: openapi.Response(
+                description="Request finished successfully.",
+                schema=schema_publisher_list_response,
+            ),
+        },
+        security=[],
         tags=["publishers"],
     )
 )
@@ -35,9 +49,21 @@ class PublisherPagination(CursorHashPagination):
 @method_decorator(
     name="retrieve",
     decorator=swagger_auto_schema(
-        operation_id="Publisher details",
+        manual_parameters=schema_publisher_details_path,
+        operation_id="publisher:details",
         operation_summary="Publisher details",
         operation_description="Allows to retrieve a details on given publisher.",
+        responses={
+            HTTP_200_OK: openapi.Response(
+                description="Request finished successfully.",
+                schema=schema_publisher_details_response,
+            ),
+            HTTP_404_NOT_FOUND: openapi.Response(
+                description="Publisher with given `id` not found.",
+                schema=schema_error("not_found"),
+            ),
+        },
+        security=[],
         tags=["publishers"],
     )
 )
