2ed26cba1e3709f5169b33a5cd08bd4aea5af1f4
Tomasz Dzieniak <tomasz.dzieniak@windly.co>
2020-10-27 15:09:53 +0100

    Implement search for books

--- bookstore/authors/admin.py
+++ bookstore/authors/admin.py
@@ -9,11 +9,11 @@ class AuthorAdmin(admin.ModelAdmin):
     """Configures application's admin panel."""
 
     # Specifies which fields will be shown at list view.
-    list_display = ("first_name", "last_name",)
+    list_display = ("first_name", "last_name")
 
     # Configures admin panel's pagination.
     list_per_page = 100
 
     # Defines searching and sorting.
-    search_fields = ("first_name", "last_name",)
-    sortable_by = ("first_name", "last_name",)
+    search_fields = ("first_name", "last_name")
+    sortable_by = ("first_name", "last_name")
--- bookstore/books/urls.py
+++ bookstore/books/urls.py
@@ -1,9 +1,14 @@
-from bookstore.books.views import BookViewSet
 from rest_framework.routers import DefaultRouter
+
+from bookstore.books.views import BookSearchViewSet
 
 app_name = "books"
 
 router = DefaultRouter()
-router.register("", BookViewSet)
+router.register(
+    basename="books",
+    prefix="",
+    viewset=BookSearchViewSet
+)
 
 urlpatterns = router.urls
