c4e57e5d4bff7ea8ad5219ac2ba9a3d62adeb8b8
Tomasz Dzieniak <tomasz.dzieniak@windly.co>
2020-11-11 20:04:45 +0100

    Add model that represents cart item

--- bookstore/carts/admin.py
+++ bookstore/carts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.admin import register
 
-from bookstore.carts.models import Cart
+from bookstore.carts.models import Cart, CartItem
 
 
 @register(Cart)
@@ -15,5 +15,22 @@ class CartAdmin(admin.ModelAdmin):
     list_per_page = 100
 
     # Defines searching and sorting.
-    search_fields = ("user", "token", "mobile", "email")
+    search_fields = ("user__email", "user__first_name", "user__last_name",
+                     "token", "mobile", "email")
     sortable_by = ("user", "created", "updated")
+
+
+@register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    """Configures application's admin panel."""
+
+    # Specifies which fields will be shown at list view.
+    list_display = ("cart", "book", "price", "discount", "quantity", "active")
+
+    # Configures admin panel's pagination.
+    list_per_page = 100
+
+    # Defines searching and sorting.
+    search_fields = ("book__title", "book__author__first_name",
+                     "book__author__last_name", "book__publisher__name")
+    sortable_by = ("cart", "price", "discount", "quantity", "active")
--- bookstore/carts/models.py
+++ bookstore/carts/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from bookstore.books.models import Book
+
 
 class CartStatus(Enum):
     """Describes a cart status."""
@@ -59,3 +61,40 @@ class Cart(models.Model):
 
         # Concatenate user and token.
         return "{} - {}".format(self.user, self.token)
+
+
+class CartItem(models.Model):
+    """Represents a single cart item."""
+
+    # Reference to identify the book associated with a cart item.
+    book = models.ForeignKey(
+        to=Book, on_delete=models.CASCADE, related_name="cart_items")
+
+    # Reference to identify the cart associated with a cart item.
+    cart = models.ForeignKey(
+        to=Cart, on_delete=models.CASCADE, related_name="cart_items")
+
+    # The stock keeping unit used while purchasing the item.
+    sku = models.CharField(max_length=50, null=True)
+
+    # The price of the item while purchasing the item.
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    # The discount of the product while purchasing the item.
+    discount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    # The quantity of the item selected by user.
+    quantity = models.SmallIntegerField(default=0)
+
+    # The flag to identify whether a product is active on the cart.
+    active = models.BooleanField(default=True)
+
+    # Availability details.
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Represents this entity as a char sequence."""
+
+        # Concatenate user and token.
+        return "{} - {}".format(self.cart, self.book)
